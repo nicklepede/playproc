766ca4e3d420d3f5d1e682a466ec3883e156e797
Andrew Wheeler <andrew@igmo.org>
2012-09-15 16:03:43 -0500

    Initial Commit

---
+++ playproc/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  playproc
+//
+//  Created by Andrew Wheeler on 9/15/12.
+//  Copyright (c) 2012 Andrew Wheeler. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
